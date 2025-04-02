03b44bc23cf2abb6a8f0328c598795cb733abb8c
Eurydia <crossdeltavi@gmail.com>
2025-04-02 22:57:56 +0200

    experiment on integration with react flow

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,40 +1,70 @@
-import { ClassFigure } from "@/components/ClassFigure";
-import { createUMLClass } from "@/services/gen";
-import { DragDropProvider } from "@dnd-kit/react";
-import { Container, Grid } from "@mui/material";
-import { useState } from "react";
+import { Box, Container } from "@mui/material";
+import {
+  applyEdgeChanges,
+  applyNodeChanges,
+  Background,
+  Edge,
+  EdgeChange,
+  Node,
+  NodeChange,
+  ReactFlow,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import { useCallback, useState } from "react";
+const initNodes: Node[] = [
+  {
+    id: "1",
+    data: { label: "Hello" },
+    position: { x: 0, y: 0 },
+    type: "input",
+  },
+  {
+    id: "2",
+    data: { label: "World" },
+    position: { x: 100, y: 100 },
+  },
+];
 
+const initEdges: Edge[] = [
+  {
+    id: "1-2",
+    source: "1",
+    target: "2",
+    type: "smoothstep",
+  },
+];
 export const App = () => {
-  const [classes, setClasses] = useState(createUMLClass(2));
-  console.debug(classes);
+  const [nodes, setNodes] = useState(initNodes);
+  const [edges, setEdges] = useState(initEdges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
   return (
     <Container>
-      <DragDropProvider>
-        <Grid
-          container
-          columns={2}
-        >
-          {classes.map((item, index) => (
-            <Grid
-              key={"class-figure" + index}
-              size={1}
-            >
-              <ClassFigure
-                classData={item}
-                onAdd={() => {
-                  // setClasses((prev) => {
-                  //   const next = [...prev];
-                  //   next[index].attributes.push(
-                  //     ...createClassAttributes(1)
-                  //   );
-                  //   return next;
-                  // });
-                }}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </DragDropProvider>
+      <Box sx={{ height: "90vh" }}>
+        <Box sx={{ height: "100%" }}>
+          <ReactFlow
+            nodes={nodes}
+            onNodesChange={onNodesChange}
+            edges={edges}
+            onEdgesChange={onEdgesChange}
+            fitView
+            snapToGrid
+            snapGrid={[25, 25]}
+          >
+            <Background gap={25} />
+            {/* <Controls /> */}
+          </ReactFlow>
+        </Box>
+      </Box>
     </Container>
   );
 };
