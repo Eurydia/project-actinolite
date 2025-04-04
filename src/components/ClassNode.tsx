7c151560e3ed33ac4463af3b832b3810730640b0
Eurydia <crossdeltavi@gmail.com>
2025-04-04 19:40:42 +0200

    added template button for class context menu

--- src/components/ClassNode.tsx
+++ src/components/ClassNode.tsx
@@ -1,12 +1,24 @@
 import { DiagramClass } from "@/types/figure";
+import { MoreVert } from "@mui/icons-material";
 import {
+  Box,
   Divider,
+  IconButton,
+  InputBase,
+  ListItemText,
+  Menu,
+  MenuItem,
   Paper,
   Stack,
-  Typography,
 } from "@mui/material";
 import { NodeResizer } from "@xyflow/react";
-import { FC, memo } from "react";
+import {
+  FC,
+  memo,
+  useCallback,
+  useRef,
+  useState,
+} from "react";
 import { ClassAttributeItem } from "./ClassAttributeItem";
 import { ClassAttributeRegion } from "./ClassAttributeRegion";
 import { MethodRegion } from "./MethodRegion";
@@ -18,12 +30,30 @@ type Props = {
 };
 export const ClassNode: FC<Props> = memo(
   ({ data, selected }) => {
+    const [name, setName] = useState(data.name);
+    const containerRef = useRef<HTMLDivElement | null>(
+      null
+    );
+    const [menuOpen, setMenuOpen] = useState(false);
+    const handleClick = useCallback(
+      ({
+        button,
+        preventDefault,
+      }: React.MouseEvent<HTMLDivElement>) => {
+        if (button === 2) {
+          preventDefault();
+          setMenuOpen(true);
+        }
+      },
+      []
+    );
     return (
       <>
         <NodeResizer
           isVisible={selected}
           minWidth={400}
         />
+
         {/* <Handle
           type="target"
           position={Position.Top}
@@ -32,7 +62,22 @@ export const ClassNode: FC<Props> = memo(
           type="source"
           position={Position.Bottom}
         /> */}
+        <Menu
+          onClose={() => setMenuOpen(false)}
+          open={menuOpen}
+          anchorEl={containerRef.current}
+          anchorOrigin={{
+            horizontal: "right",
+            vertical: "top",
+          }}
+        >
+          <MenuItem>
+            <ListItemText>sadasd</ListItemText>
+          </MenuItem>
+        </Menu>
         <Paper
+          ref={containerRef}
+          component="div"
           variant="outlined"
           sx={{
             width: "100%",
@@ -43,24 +88,60 @@ export const ClassNode: FC<Props> = memo(
             flexDirection: "column",
           }}
         >
-          <Typography
+          <Box
             sx={{
               backgroundColor: "pink",
-              fontFamily: "monospace",
-              fontWeight: 900,
               padding: 1,
               display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
+              flexDirection: "row",
+              alignItems: "flex-start",
             }}
           >
-            {data.name}
-          </Typography>
+            <IconButton
+              size="small"
+              sx={{
+                visibility: "hidden",
+              }}
+            />
+            <InputBase
+              value={name}
+              onChange={({ target }) => {
+                setName(target.value);
+              }}
+              fullWidth
+              placeholder="unnamed"
+              multiline
+              sx={{
+                display: "flex",
+                alignItems: "flex-start",
+                justifyContent: "flex-start",
+              }}
+              slotProps={{
+                input: {
+                  sx: {
+                    "&:focus": {
+                      textDecorationLine: "underline",
+                    },
+                    "fontFamily": "monospace",
+                    "whiteSpace": "normal",
+                    "overflowWrap": "break-word",
+                    "fontWeight": "bold",
+                    "textAlign": "center",
+                  },
+                  autoCorrect: "off",
+                  spellCheck: "false",
+                },
+              }}
+            />
+            <IconButton size="small">
+              <MoreVert />
+            </IconButton>
+          </Box>
           <Stack
             spacing={1}
             divider={<Divider variant="middle" />}
             sx={{
-              height: "85%",
+              height: "100%",
               overflow: "auto",
               scrollbarWidth: "thin",
             }}
